43a778a577b9126534ca23dc8ae9b5d7d0f60e95
Efrén Ruíz <efren282@outlook.es>
2022-10-10 10:22:27 -0500

    :sparkles: [ADD]: Implementation of classes (abstraction)

--- scripts/objects.js
+++ scripts/objects.js
@@ -22,11 +22,53 @@ class Student {
   }
 }
 
+class LearningPath {
+  constructor({ name, courses = [] }) {
+    this.name = name;
+    this.courses = courses;
+  }
+}
+
+class Course {
+  constructor({ name, classes = [] }) {
+    this.name = name;
+    this.classes = classes;
+  }
+}
+
+const courseProgBasica = new Course({
+  name: "Curso de Programación Básica",
+});
+
+const courseDefinitivoHTML = new Course({
+  name: "Curso Definitivo de HTML y CSS",
+});
+
+const coursePracticoHTML = new Course({
+  name: "Curso Práctico de HTML y CSS",
+});
+
+const escuelaWeb = new LearningPath({
+  name: "Escuela de Desarrollo Web",
+  courses: [courseProgBasica, courseDefinitivoHTML, coursePracticoHTML],
+});
+
+const escuelaData = new LearningPath({
+  name: "Escuela de Data Science",
+  courses: [courseProgBasica, "Curso DataBusiness", "Curso DataViz"],
+});
+
+const escuelaVideojuegos = new LearningPath({
+  name: "Escuela de Videojuegos",
+  courses: [courseProgBasica, "Curso de Unity", "Curso de Unreal Engine"],
+});
+
 const juan2 = new Student({
   name: "JuanDC",
   username: "juandc",
   email: "juanito@juanito.com",
   twitter: "fjuandc",
+  learningPaths: [escuelaWeb, escuelaVideojuegos],
 });
 
 const miguelito = new Student({
@@ -34,4 +76,5 @@ const miguelito = new Student({
   username: "miguelitofeliz",
   email: "miguelito@juanito.com",
   instagram: "miguelito_feliz",
+  learningPaths: [escuelaWeb, escuelaData],
 });
